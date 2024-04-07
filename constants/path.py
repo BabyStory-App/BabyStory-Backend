1690c8feb3b478e0a5a912b45ccb576dee587ee8
jaewonE <importjaewonE@gmail.com>
2024-04-07 23:02:49 +0900

    Refactor path.py to use os module for getting project directory and print additional debug information

--- constants/path.py
+++ constants/path.py
@@ -1,7 +1,9 @@
-from core.env import env
+import os
 
-PROJECT_DIR = env.get('PROJECT_DIR')
+PROJECT_DIR = os.getcwd()
+print(f'PROJECT_DIR: {PROJECT_DIR}')
 BABY_CRY_DATASET_DIR = f'{PROJECT_DIR}/dataset/baby_cry'
 CRY_INSPECT_LOG_DIR = f'{PROJECT_DIR}/logs/cry_inspect'
 PROFILE_IMAGE_DIR = f'{PROJECT_DIR}/dataset/profile'
 ASSET_DIR = f'{PROJECT_DIR}/assets'
+print(f'ASSET_DIR: {ASSET_DIR}')

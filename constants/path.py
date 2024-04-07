ea361226920ab633a688a25dc697da0666877474
jaewonE <importjaewonE@gmail.com>
2024-04-07 23:11:46 +0900

    Add services/cry_predict.py

--- constants/path.py
+++ constants/path.py
@@ -1,9 +1,7 @@
 import os
 
 PROJECT_DIR = os.getcwd()
-print(f'PROJECT_DIR: {PROJECT_DIR}')
 BABY_CRY_DATASET_DIR = f'{PROJECT_DIR}/dataset/baby_cry'
 CRY_INSPECT_LOG_DIR = f'{PROJECT_DIR}/logs/cry_inspect'
 PROFILE_IMAGE_DIR = f'{PROJECT_DIR}/dataset/profile'
 ASSET_DIR = f'{PROJECT_DIR}/assets'
-print(f'ASSET_DIR: {ASSET_DIR}')

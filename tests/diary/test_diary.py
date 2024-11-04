0f73c243c0a538da1a156c54a8d54efb7a180d90
gahyun <gahyun727301@kangnam.ac.kr>
2024-11-04 17:40:49 +0900

    [ Test ] test_create_diary1, test_create_diary2 in test_diary.py

--- tests/diary/test_diary.py
+++ tests/diary/test_diary.py
@@ -10,44 +10,44 @@ test_jwt_tmp = None
 test_MaternityDiary = {
     "baby_id": "test_baby_id",
     "born": 0,
-    "title": "Maternity Diary"
+    "title": "산모수첩"
 }
 
 test_ParentingDiary = {
     "baby_id": "test_baby_id",
     "born": 1,
-    "title": "Parenting Diary"
+    "title": "육아일기"
 }
 
 
 """ Create diary """
 # 산모수첩 생성
 def test_create_diary1(client, test_jwt):
+    test_MaternityDiary["baby_id"] = test_jwt["foetus"]
     response = client.post(
         "/diary/create",
         headers={"Authorization": f"Bearer {test_jwt['access_token']}"},
         json=test_MaternityDiary
     )
-    test_MaternityDiary["baby_id"] = test_jwt["foetus"]
-
+    
     assert response.status_code == 200
     response_json = response.json()
     assert "diary" in response_json
 
-    assert response_json["diary"]["parent_id"] == test_jwt["access_token"]
+    assert response_json["diary"]["baby_id"] == test_jwt["foetus"]
 
 
 # 육아일기 생성
 def test_create_diary2(client, test_jwt):
+    test_ParentingDiary["baby_id"] = test_jwt["baby"]
     response = client.post(
         "/diary/create",
         headers={"Authorization": f"Bearer {test_jwt['access_token']}"},
         json=test_ParentingDiary
     )
-    test_ParentingDiary["baby_id"] = test_jwt["baby"]
 
     assert response.status_code == 200
     response_json = response.json()
     assert "diary" in response_json
 
-    assert response_json["diary"]["parent_id"] == test_jwt["access_token"]
+    assert response_json["diary"]["baby_id"] == test_jwt["baby"]

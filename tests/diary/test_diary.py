c1470bc579d1841fdec4e4c4adcad28c32468982
gahyun <gahyun727301@kangnam.ac.kr>
2024-11-04 17:59:14 +0900

    [ Test ] test_upload_diary_cover_image in test_diary.py

--- tests/diary/test_diary.py
+++ tests/diary/test_diary.py
@@ -16,7 +16,11 @@ test_MaternityDiary = {
 test_ParentingDiary = {
     "baby_id": "test_baby_id",
     "born": 1,
-    "title": "육아일기"
+    "title": "육아일지"
+}
+
+test_cover = {
+    "file": "test1.png"
 }
 
 
@@ -51,3 +55,26 @@ def test_create_diary2(client, test_jwt):
     assert "diary" in response_json
 
     assert response_json["diary"]["baby_id"] == test_jwt["baby"]
+    test_jwt["parenting_diary"] = response_json["diary"]["diary_id"]
+
+
+""" upload diary cover image """
+def test_upload_diary_cover_image(client, test_jwt):
+    file = [
+        ("file", (test_cover["file"], open(os.path.join(TEST_ASSET_DIR, test_cover["file"]), "rb")))
+    ]
+
+    response = client.post(
+        f"/diary/coverUpload/{test_jwt['parenting_diary']}",
+        headers={"Authorization": f"Bearer {test_jwt['access_token']}"},
+        files=file,
+        data={"diary_id": test_jwt["parenting_diary"]}
+    )
+
+    assert response.status_code == 200
+    response_json = response.json()
+    assert "success" in response_json
+
+    file_type = test_cover["file"].split(".")[-1]
+    assert create_file_exist(os.path.join(
+        DIARY_COVER_PATH, f"{test_jwt['parenting_diary']}.{file_type}"))

165adc1181132f6fa0cd8fc1c4867f3693bd5785
gahyun <gahyun727301@kangnam.ac.kr>
2024-11-04 14:12:18 +0900

    [ Fix ] test code test_get_scripts in test_setting.py

--- tests/setting/test_setting.py
+++ tests/setting/test_setting.py
@@ -70,7 +70,7 @@ def test_get_my_views(client, test_jwt):
     assert "paginationInfo" in response_json
 
     assert response_json["post"][0]["post_id"] == test_jwt["post_id"]
-    assert len(response_json["post"][0]["contentPreview"]) <= 100
+    assert len(response_json["post"][0]["contentPreview"]) <= 101
 
 # test_get_my_views ( 잘못된 jwt )
 async def test_get_my_views_fail():
@@ -93,8 +93,8 @@ def test_get_scripts(client, test_jwt):
     assert "post" in response_json
     assert "paginationInfo" in response_json
 
-    assert response_json["post"]["post_id"] == test_jwt["post_id"]
-    assert len(response_json["post"]["contentPreview"]) == 100
+    assert response_json["post"][0]["post_id"] == 1
+    assert len(response_json["post"][0]["contentPreview"]) <= 101
 
 # test_get_scripts ( 잘못된 jwt )
 async def test_get_scripts_fail():
